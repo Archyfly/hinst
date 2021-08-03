245ab7e7d860353e54a4162ae10542e3cf351b69
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-03 16:56:14 +0800

    add actions for Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,30 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: %i[new]
+  before_action :find_post
 
   def index
     @comments = post.comments.all
   end
 
   def new
-    @comment = post.comments.create
+    @comment = post.comments.new
+  end
+
+  def create
+    @comment = post.comments.create(comment_params)
+    if @comment.save
+      redirect_to post_path
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:description)
   end
 
 end
