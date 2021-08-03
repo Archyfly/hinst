a751516cb34dfd55e8251219420f9b5de237d5c5
Mikhail Kashinin <flyaway400@gmail.com>
2021-08-03 16:48:59 +0800

    create users controller. CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: %i[new edit update destroy]
 
   def index
     @posts = Post.all
